817e12894abe9baa7c1cdd5a0330dc7ca20c4e2e
Olesia Pashko <pashkoolesia1234@gmail.com>
2020-12-13 15:47:00 +0200

    change project structure

--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -6,7 +6,12 @@ public class Player : MonoBehaviour
 {
     void Start()
     {
-        
+        Rect rect = new Rect(transform.position, transform.localScale);
+        Debug.Log(rect);
+        Debug.Log(transform.position.x - transform.localScale.x / 2);
+        Debug.Log(transform.position.x + transform.localScale.x / 2);
+        Debug.Log(transform.position.y - transform.localScale.y / 2);
+        Debug.Log(transform.position.y + transform.localScale.y / 2);
     }
 
     // Update is called once per frame
